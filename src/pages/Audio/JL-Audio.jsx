2a66f1ced9488f9d03bfe8572a1d41d289c35182
Logan McCalley <mccalleylogan@gmail.com>
2022-12-06 13:53:53 -0500

    cors error

--- src/pages/Audio/JL-Audio.jsx
+++ src/pages/Audio/JL-Audio.jsx
@@ -1,12 +1,49 @@
+import { useState, useEffect } from 'react'
 import '../../styles/Products.css'
 
 function JL(){
+
+    const[speakers, setSpeakers] = useState([])
+
+    useEffect(() =>{
+        fetch('https://marine-store-api.web.app/JL-Audio')
+        .then(results => results.json())
+        .then(jlSpeakers => setSpeakers(jlSpeakers))
+        .catch(err => console.error(err))
+    }, [setSpeakers])
+
+    if(!speakers){
+        return <h2>Loading...</h2>
+      }
+
 
     return(
         <div>
             <div className="product-header" id="jl-header"></div>
             <div className='products'>
                 <h1 className='product-name'>Speakers</h1>
+                <ul className="product-container">
+            
+            {speakers.map(jlSpeakers => (
+                <div key={jlSpeakers._id} className="productCards">
+                         {/* {crew.profilePic && <img className="profilePic"  src={crew.profilePic} width={300} />} */}
+                        <div className="text-area">
+                         <div className="Name">
+                         {jlSpeakers.name}
+                         </div>
+                          {/* <a href={crew.Instagram} >
+                          <FontAwesomeIcon icon={faInstagram} size="2x"/>
+                          </a> */}
+                         <div className="description">
+                         {jlSpeakers.description}
+                         </div>
+                         {/* <button>Delete</button> */}
+                        </div>
+                </div>
+            ))}
+            {/* </div> */}
+        </ul>
+       
             </div>
             <div className='products'>
                 <h1 className='product-name'>Subwoofers</h1>
