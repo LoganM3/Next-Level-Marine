975825a7b869cc944472b5b7061780e1e3703282
Logan McCalley <mccalleylogan@gmail.com>
2022-12-08 12:06:50 -0500

    start of product cards

--- src/pages/Audio/JL-Audio.jsx
+++ src/pages/Audio/JL-Audio.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Spinner from 'react-bootstrap/Spinner'
 import '../../styles/Products.css'
 
 function JL(){
@@ -13,7 +14,11 @@ function JL(){
     }, [setSpeakers])
 
     if(!speakers){
-        return <h2>Loading...</h2>
+        return (
+            <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        )
       }
 
 
@@ -21,29 +26,26 @@ function JL(){
         <div>
             <div className="product-header" id="jl-header"></div>
             <div className='products'>
-                <h1 className='product-name'>Speakers</h1>
+              <h1 className='product-name'>Speakers</h1>
                 <ul className="product-container">
-            
             {speakers.map(jlSpeakers => (
-                <div key={jlSpeakers._id} className="productCards">
-                         {/* {crew.profilePic && <img className="profilePic"  src={crew.profilePic} width={300} />} */}
-                        <div className="text-area">
+                <div key={jlSpeakers.id} className="productCards">
+                         {/* {jlSpeakers.profilePic && <img className="profilePic"  src={jlSpeakers.profilePic} width={300} />} */}
+                         <div className="text-area">
                          <div className="Name">
                          {jlSpeakers.name}
                          </div>
-                          {/* <a href={crew.Instagram} >
-                          <FontAwesomeIcon icon={faInstagram} size="2x"/>
-                          </a> */}
+                         <br/>
                          <div className="description">
                          {jlSpeakers.description}
                          </div>
-                         {/* <button>Delete</button> */}
+                         <br/>
+                         ${jlSpeakers.price}
                         </div>
+                        <button>Add to cart</button>
                 </div>
             ))}
-            {/* </div> */}
         </ul>
-       
             </div>
             <div className='products'>
                 <h1 className='product-name'>Subwoofers</h1>
